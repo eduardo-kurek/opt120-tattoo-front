a8fb18c3f5446333953c7e12db7a6a52bbb8a2be
eduardo-kurek <eduardokurek2003@gmail.com>
2024-07-31 21:18:48 -0300

    Visual da tela de cadastro

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,9 +1,23 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'components/menu.dart';
 
-class Register extends StatelessWidget {
+class Register extends StatefulWidget {
   const Register({super.key});
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void register(){
+    print(_nameController.text);
+    print(_passwordController.text);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,11 +28,40 @@ class Register extends StatelessWidget {
       ),
       body: Container(
         color: Colors.black87,
-        child: const Center(
+        padding: const EdgeInsets.all(10),
+        child: Center(
           child: Card(
             child: Padding(
-              padding: EdgeInsets.all(15),
-              child: Text("OLÁ MUNDO"),
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text("Cadastrar", style: TextStyle(fontSize: 25)),
+                  const Divider(height: 50, indent: 10),
+                  TextFormField(
+                    controller: _nameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Usuário',
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.person)
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: _passwordController,
+                    decoration: const InputDecoration(
+                        labelText: 'Senha',
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.password)
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                      onPressed: register,
+                      child: const Text("Cadastrar", style: TextStyle(fontSize: 17))
+                  )
+                ],
+              ),
             ),
           )
         ),
