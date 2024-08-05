5811efe84022bf0032532853f4b355fd4108711e
thiagogquinto <thiagogarianiq@gmail.com>
2024-08-05 16:36:04 -0300

    add navigations and start Home Page

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tatuagem_front/services/Api.dart';
 import 'package:tatuagem_front/utils/Messenger.dart';
 import 'components/menu.dart';
+import 'home.dart';
 
 class Register extends StatefulWidget {
   const Register({super.key});
@@ -14,13 +15,21 @@ class _RegisterState extends State<Register> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
-  void register() async{
+  void register() async {
     try {
       final data = await ApiService.post('auth/register', {
         'nome_usuario': _nameController.text,
         'senha': _passwordController.text
       });
+
       Messenger.snackBar(context, data["message"]);
+
+      if (data['statusCode'] == 201) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage()),
+        );
+      }
     } catch (e) {
       Messenger.snackBar(context, e.toString());
     }
@@ -29,15 +38,15 @@ class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: const Menu(),
-      appBar: AppBar(
-        title: const Text("Cadastrar"),
-      ),
-      body: Container(
-        color: Colors.black87,
-        padding: const EdgeInsets.all(50),
-        child: Center(
-          child: Card(
+        drawer: const Menu(),
+        appBar: AppBar(
+          title: const Text("Cadastrar"),
+        ),
+        body: Container(
+          color: Colors.black87,
+          padding: const EdgeInsets.all(50),
+          child: Center(
+              child: Card(
             child: Padding(
               padding: const EdgeInsets.all(20.0),
               child: Column(
@@ -48,9 +57,9 @@ class _RegisterState extends State<Register> {
                   TextFormField(
                     controller: _nameController,
                     decoration: const InputDecoration(
-                      labelText: 'Usuário',
-                      border: OutlineInputBorder(),
-                      prefixIcon: Icon(Icons.person)
+                        labelText: 'Usuário',
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.person)
                     ),
                   ),
                   const SizedBox(height: 20),
@@ -65,14 +74,11 @@ class _RegisterState extends State<Register> {
                   const SizedBox(height: 20),
                   ElevatedButton(
                       onPressed: register,
-                      child: const Text("Cadastrar", style: TextStyle(fontSize: 17))
-                  )
+                      child: const Text("Cadastrar", style: TextStyle(fontSize: 17)))
                 ],
               ),
             ),
-          )
-        ),
-      )
-    );
+          )),
+        ));
   }
 }
