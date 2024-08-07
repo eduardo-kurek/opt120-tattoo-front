61778c1ad1d6b8dd6cc1de27f83a288189f3a1e5
eduardo-kurek <eduardokurek2003@gmail.com>
2024-08-07 18:33:03 -0300

    Protegendo páginas de usuário

--- lib/screens/user/home.dart
+++ lib/screens/user/home.dart
@@ -1,29 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:tatuagem_front/screens/components/authenticate.dart';
 import 'package:tatuagem_front/screens/components/user_menu.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Page'),
-      ),
-      drawer: const UserMenu(),
-      body: Container(
-        color: Colors.black87,
-        padding: const EdgeInsets.all(50),
-        child: Center(
-            child: Card(
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Text("Conteudo aqui...")
-                  ]
+    return Authenticate(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Home Page'),
+        ),
+        drawer: const UserMenu(),
+        body: Container(
+          color: Colors.black87,
+          padding: const EdgeInsets.all(50),
+          child: const Center(
+              child: Card(
+                child: Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text("Conteudo aqui...")
+                    ]
+                  ),
                 ),
-              ),
-            )
+              )
+          ),
         ),
       ),
     );
--- lib/screens/user/my_account.dart
+++ lib/screens/user/my_account.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:tatuagem_front/screens/components/authenticate.dart';
 
 class MyAccount extends StatelessWidget {
   const MyAccount({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Authenticate(child: Placeholder());
   }
 }
