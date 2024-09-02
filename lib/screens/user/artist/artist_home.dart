06676a0c8c1cd373bc1033c160684f165287ea53
eduardo-kurek <eduardokurek2003@gmail.com>
2024-09-02 19:35:41 -0300

    Corrigindo tela artist_home

--- lib/screens/user/artist/artist_home.dart
+++ lib/screens/user/artist/artist_home.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
-import 'package:tatuagem_front/DAO/TattooDAO.dart';
 import 'package:tatuagem_front/DAO/ScheduleDAO.dart';
+import 'package:tatuagem_front/DAO/TattooDAO.dart';
 import 'package:tatuagem_front/Models/Schedule.dart';
+import 'package:tatuagem_front/Models/Tattoo.dart';
 import 'package:tatuagem_front/screens/components/authenticate.dart';
 import 'package:tatuagem_front/screens/components/menu.dart';
 import 'package:tatuagem_front/screens/components/user_menu.dart';
-import 'package:tatuagem_front/utils/Messenger.dart';
-
-import '../../../Models/Tattoo.dart';
-import '../../../utils/TokenProvider.dart';
+import 'package:tatuagem_front/utils/TokenProvider.dart';
 
 class ArtistHome extends StatefulWidget {
   const ArtistHome({super.key});
@@ -20,27 +17,23 @@ class ArtistHome extends StatefulWidget {
 }
 
 class _ArtistHomeState extends State<ArtistHome> {
-  List<Schedule> _tattoos = [];
+  List<Schedule> _schedules = [];
 
-  Future<void> _cancelScheduling(String tattooId) async {
+  void _refreshData() async {
     final tokenProvider = Provider.of<TokenProvider>(context, listen: false);
     ScheduleDAO dao = ScheduleDAO(tokenProvider: tokenProvider);
-    try {
-      await dao.delete(tattooId);
-      _refreshData();
-    } catch (e) {
-      Messenger.snackBar(context, 'Erro ao cancelar agendamento');
-    }
 
+    _schedules = await dao.getAllByArtistUser();
+    setState(() {});
   }
 
-  void _refreshData() async{
+  void _cancelar(String agendamentoId) async{
     final tokenProvider = Provider.of<TokenProvider>(context, listen: false);
     ScheduleDAO dao = ScheduleDAO(tokenProvider: tokenProvider);
-    var decoded = tokenProvider.decodedToken;
 
-    _tattoos = await dao.getAllByArtistUser();
-    setState(() {});
+    await dao.delete(agendamentoId);
+
+    _refreshData();
   }
 
   @override
@@ -49,120 +42,52 @@ class _ArtistHomeState extends State<ArtistHome> {
     _refreshData();
   }
 
-   @override
+  @override
   Widget build(BuildContext context) {
     return Authenticate(
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('Meus Agendamentos'),
+          title: const Text('Agendamentos realizados'),
         ),
         drawer: const Menu(),
         body: Container(
           color: Colors.black87,
-          padding: const EdgeInsets.all(15),
-          child: LayoutBuilder(builder: (context, constraints) {
-            // Calcula o número de colunas com base na largura da tela
-            int crossAxisCount = (constraints.maxWidth / 200).floor();
-            return GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: crossAxisCount, // Número de colunas
-                  crossAxisSpacing: 20.0,
-                  mainAxisSpacing: 20.0,
-                  childAspectRatio: 0.6, // Proporção do card (largura/altura)
-                ),
-                itemCount: _tattoos.length,
-                itemBuilder: (context, index) {
-                  final tatoo = _tattoos[index];
-                  return TatooCard(
-                    tatoo: tatoo,
-                    onCancel: (tattooId) => _cancelScheduling(tattooId),
-                  );
-                });
-          }),
-        ),
-      ),
-    );
-  }
-}
-
-class TatooCard extends StatelessWidget {
-  const TatooCard({
-    super.key,
-    required this.tatoo,
-    required this.onCancel,
-  });
-
-  final Schedule tatoo;
-  final void Function(String tattooId) onCancel;
-
-  @override
-  Widget build(BuildContext context) {
-    print('tatoo.imagem: ${tatoo.imagem}');
-    return Card(
-      elevation: 4.0,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Flexible(
-            flex: 1,
-            child: Image.network(
-              tatoo.imagem,
-              width: double.infinity,
-              fit: BoxFit.cover,
-              errorBuilder: (BuildContext context, Object exception,
-                  StackTrace? stackTrace) {
-                return Placeholder();
-              },
+          padding: const EdgeInsets.all(50),
+          child: ListView.builder(
+            itemCount: _schedules.length,
+            itemBuilder: (context, i) => Card(
+                child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        ListTile(
+                          title: Text(_schedules[i].estilo ?? 'Estilo'),
+                          subtitle: Text(_schedules[i].preco?.toStringAsFixed(2) ?? 'Preço'),
+                        ),
+                        const Divider(height: 10),
+                        Image.network(
+                          _schedules[i].imagem ?? '',
+                          width: double.infinity,
+                          height: 400,
+                          fit: BoxFit.cover,
+                          errorBuilder: (BuildContext context, Object exception,
+                              StackTrace? stackTrace) {
+                            return Text("Erro ao buscar imagem");
+                          },
+                        ),
+                        const Divider(height: 10),
+                        TextButton(
+                            onPressed: () {
+                              _cancelar(_schedules[i].agendamento_id);
+                            },
+                            child: Text('Cancelar agendamento')
+                        )
+                      ],
+                    )
+                )
             ),
           ),
-          Flexible(
-              flex: 1,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'R\$ ${tatoo.preco.toString().replaceAll('.', ',')}',
-                      style: const TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Text(
-                      'Tamanho: ${tatoo.tamanho.toString()} cm',
-                      style: TextStyle(
-                        fontSize: 14.0,
-                        color: Colors.grey[600],
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Text(
-                      'Estilo: ${tatoo.estilo}',
-                      style: TextStyle(
-                        fontSize: 14.0,
-                        color: Colors.grey[600],
-                      ),
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                       IconButton(
-                      icon: const Icon(Icons.event_busy),
-                      color: Colors.red,
-                      onPressed: () => onCancel(tatoo.id!),
-                    ),
-                    ],
-                  )
-                ],
-              )),
-        ],
+        ),
       ),
     );
   }
